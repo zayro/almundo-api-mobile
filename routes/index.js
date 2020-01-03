99937fbba8af22e512454ddb69c58ea40c4ba1be
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2020-01-03 03:00:02 -0500

    documentacion

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ import { validateBody } from '../middleware/validate'
 
 const api = express.Router()
 
+/**
+ * configuracion de las rutas del Api
+ */
+
 api.get('/hotel',  hotelCtrl.list)
 api.get('/hotel/:id',  hotelCtrl.read)
 api.post('/hotel', validateBody(['name', 'description', 'images', 'starts', 'price']),  hotelCtrl.create)
